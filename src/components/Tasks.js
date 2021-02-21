73466f89ce4b9416d50554611a6e46fa25749879
Leo <leocsdev@gmail.com>
2021-02-21 18:44:43 +0800

    Made state as global state by moving tasks state in App component.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,31 +1,4 @@
-// State and useState Hook
-// 1. To use state in a functional component, import useState 
-import { useState } from 'react'
-
-const Tasks = () => {
-  // 2. and add useState inside the functional component (temporarily to show how it works)
-  // 3. Tasks in state is immutable, use setTasks to update a task instead
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctor\'s Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at school',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
-  ])
-
+const Tasks = ({ tasks }) => {
   return (
     <>
       {
